f6d3447dcecc5d3f9bcc3495bb7d4485aa1d6b56
Dylan Alexander <dylanp.arango12@gmail.com>
2024-07-03 22:30:38 -0500

    Added: Header, Hero & Companies Section

---
+++ src/info/companies.js
@@ -0,0 +1,29 @@
+const companies = [
+  {
+    name: "Horizon",
+    url: "horizonagency.xyz",
+    photo: "/Horizon.png",
+  },
+  {
+    name: "Betteru",
+    url: "",
+    photo: "/Betteru.png",
+  },
+  {
+    name: "MakeCode",
+    url: "",
+    photo: "/MakeCode.png",
+  },
+  {
+    name: "Supremacy",
+    url: "supremacycs.com",
+    photo: "/Supremacy.png",
+  },
+  {
+    name: "",
+    url: "texdesign.co",
+    photo: "/Texdesign.png",
+  },
+];
+
+export default companies;
