ddf1c341a85a6883ed0d53ec4ab02e7a32e2d7ed
Dylan Alexander <dylanp.arango12@gmail.com>
2024-07-04 03:29:42 -0500

    Added: Trayectory

--- src/info/companies.js
+++ src/info/companies.js
@@ -3,36 +3,50 @@ const companies = [
     name: "Horizon",
     url: "https://horizonagency.xyz/",
     photo: "/Horizon.png",
+    description:
+      "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
   },
   {
     name: "Betteru",
     url: "",
     photo: "/Betteru.png",
+    description:
+      "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
   },
   {
     name: "MakeCode",
     url: "",
     photo: "/MakeCode.png",
+    description:
+      "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
   },
   {
     name: "Supremacy",
     url: "https://supremacycs.com",
     photo: "/Supremacy.png",
+    description:
+      "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
   },
   {
     name: "CoderLabs",
     url: "https://coderlabs.co",
     photo: "/CoderLabs.png",
+    description:
+      "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
   },
   {
     name: "ContentCraft",
     url: "",
     photo: "/Contentcraft.png",
+    description:
+      "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
   },
   {
     name: "RISE",
     url: "https://risejna.vercel.app",
     photo: "/Rise.png",
+    description:
+      "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
   },
 ];
 
