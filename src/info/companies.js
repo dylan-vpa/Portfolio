8796ccc107dd0110f7cc5f2f20f320e02256066b
Dylan Alexander <dylanp.arango12@gmail.com>
2024-07-05 19:46:20 -0500

    Added: Companies, Events & Blog page

--- src/info/companies.js
+++ src/info/companies.js
@@ -1,50 +1,56 @@
 const companies = [
   {
     name: "Horizon",
+    position: "CEO | Founder",
     url: "https://horizonagency.xyz/",
     photo: "/Horizon.png",
+    colorPhoto: "/colorHorizon.png",
     description:
       "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
   },
   {
     name: "Betteru",
+    position: "CEO | Founder",
     url: "",
     photo: "/Betteru.png",
+    colorPhoto: "/ColorBetteru.png",
     description:
       "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
   },
   {
     name: "MakeCode",
+    position: "CEO | Founder",
     url: "",
     photo: "/MakeCode.png",
+    colorPhoto: "/colorMakeCode.png",
     description:
       "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
   },
   {
     name: "Supremacy",
+    position: "Partner",
     url: "https://supremacycs.com",
     photo: "/Supremacy.png",
+    colorPhoto: "/colorSupremacy.png",
+    colorPhoto: "/colorSupremacy.png",
     description:
       "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
   },
   {
     name: "CoderLabs",
+    position: "Businesss Manager",
     url: "https://coderlabs.co",
     photo: "/CoderLabs.png",
+    colorPhoto: "/colorCoderLabs.png",
     description:
       "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
   },
   {
     name: "ContentCraft",
+    position: "Contentcraft Expert | Founder",
     url: "",
     photo: "/Contentcraft.png",
-    description:
-      "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
-  },
-  {
-    name: "RISE",
-    url: "https://risejna.vercel.app",
-    photo: "/Rise.png",
+    colorPhoto: "/colorContentcraft.png",
     description:
       "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
   },
