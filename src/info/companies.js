8a1e5eabbfbf9383f07589ed60cdf385e5e7b267
Dylan Alexander <dylanp.arango12@gmail.com>
2024-07-07 13:50:05 -0500

    Added: Info & Copies

--- src/info/companies.js
+++ src/info/companies.js
@@ -6,7 +6,7 @@ const companies = [
     photo: "/Horizon.png",
     colorPhoto: "/colorHorizon.png",
     description:
-      "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
+      "En 2020, fundé Horizon, una agencia de capital privado buscnado consolidarla como líder en impulsar el crecimiento digital de empresas emergentes y en expansión. En Horizon, identificamos oportunidades de crecimiento prometedoras y proporcionamos las herramientas, la experiencia y los recursos necesarios para que nuestros socios alcancen su máximo potencial. Nuestra especialización en estrategias de crecimiento digital nos permite guiar a las empresas a través de la transformación digital, maximizando su eficiencia operativa y su alcance en el mercado.",
   },
   {
     name: "Betteru",
@@ -15,7 +15,7 @@ const companies = [
     photo: "/Betteru.png",
     colorPhoto: "/ColorBetteru.png",
     description:
-      "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
+      "Betteru, lanzada en 2024, es una plataforma de comercio electrónico que se busca convertirse en un referente en el ámbito del crecimiento personal. Nuestra misión es proporcionar a nuestros clientes las herramientas y los recursos necesarios para alcanzar su máximo potencial en todas las áreas de sus vidas. Ofrecemos una amplia gama de productos y servicios cuidadosamente seleccionados. En BetterU, creemos que el crecimiento personal es un viaje continuo y estamos comprometidos a acompañar a nuestros clientes en cada paso del camino.",
   },
   {
     name: "MakeCode",
@@ -24,17 +24,17 @@ const companies = [
     photo: "/MakeCode.png",
     colorPhoto: "/colorMakeCode.png",
     description:
-      "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
+      "MakeCode, fundada en 2023, es una academia de desarrollo de software que se distingue por su enfoque en los fundamentos STEAM (Ciencia, Tecnología, Ingeniería, Artes y Matemáticas). A través de programas educativos innovadores y prácticos, MakeCode empodera a estudiantes de todas las edades para que se conviertan en creadores y solucionadores de problemas, utilizando la tecnología como herramienta para dar vida a sus ideas. Nuestra metodología, basada en proyectos y aprendizaje colaborativo, fomenta el pensamiento crítico, la creatividad y la pasión por el aprendizaje continuo.",
   },
   {
-    name: "Supremacy",
+    name: "Skyline",
     position: "Partner",
     url: "https://supremacycs.com",
     photo: "/Supremacy.png",
     colorPhoto: "/colorSupremacy.png",
     colorPhoto: "/colorSupremacy.png",
     description:
-      "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
+      "Skyline, establecida en 2023, es una marca de ropa streetwear que ha capturado la esencia de la cultura urbana y la autoexpresión. Nuestras colecciones, diseñadas con pasión y autenticidad, reflejan las últimas tendencias en moda urbana, fusionando estilo y comodidad. Skyline se ha convertido en un símbolo de identidad para aquellos que buscan destacarse y expresar su individualidad a través de la moda. Colaboramos con artistas y creadores emergentes para ofrecer diseños únicos y exclusivos que resuenan con la comunidad streetwear.",
   },
   {
     name: "CoderLabs",
@@ -43,7 +43,7 @@ const companies = [
     photo: "/CoderLabs.png",
     colorPhoto: "/colorCoderLabs.png",
     description:
-      "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
+      "CoderLabs, fundada en 2021, es una agencia de desarrollo de software que se especializa en crear soluciones tecnológicas innovadoras y a medida para empresas de todos los tamaños y sectores. Nuestro equipo de expertos en desarrollo de software trabaja en estrecha colaboración con nuestros clientes para comprender sus necesidades y objetivos, y luego diseñar e implementar soluciones que impulsen su crecimiento y eficiencia. En CoderLabs, nos apasiona la tecnología y su capacidad para transformar negocios, y nos enorgullece ofrecer servicios de alta calidad que superan las expectativas de nuestros clientes.",
   },
   {
     name: "ContentCraft",
@@ -52,7 +52,7 @@ const companies = [
     photo: "/Contentcraft.png",
     colorPhoto: "/colorContentcraft.png",
     description:
-      "Hola, soy un joven desarrollador web que busca seguir aprendiendo para ampliar sus horizontes. Me apasiona la música, el café y la tecnología. Gracias por el apoyo.",
+      "ContentCraft, fundada en 2023, es una agencia digital que se ha posicionado como líder en la creación de estrategias de contenido efectivas e innovadoras. Nuestro equipo multidisciplinario de expertos en marketing digital, diseño gráfico y redacción trabaja en conjunto para desarrollar soluciones integrales que impulsen el crecimiento y la visibilidad de nuestros clientes en el mundo digital. En ContentCraft, entendemos que el contenido es el corazón de cualquier estrategia de marketing exitosa, y nos apasiona ayudar a nuestros clientes a contar sus historias de manera auténtica y atractiva.",
   },
 ];
 
